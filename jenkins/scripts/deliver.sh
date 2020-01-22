ca16e1446a90bdccba76e54b5732a92ca2bd441e
ericsonrumuy7 <48756749+ericsonrumuy7@users.noreply.github.com>
2020-01-22 14:24:03 +0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-scp -P 2209 target/${NAME}-${VERSION}.jar linux1-user@jenkins-future-2.infra.lokal:/home/linux1-user
-#java -jar target/${NAME}-${VERSION}.jar
+scp -P 2209 target/${NAME}-${VERSION}.jar linux1-user@$1:/home/linux1-user
+java -jar target/${NAME}-${VERSION}.jar
